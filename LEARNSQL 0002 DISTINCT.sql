74591eaee27eec1f406fb9d55b3919487b03bb38
Nikhil Sharma <159870552+Nikhil9Sharma@users.noreply.github.com>
2026-02-23 18:39:17 +0530

    Distinct and group by learn

--- LEARNSQL 0002 DISTINCT.sql
+++ LEARNSQL 0002 DISTINCT.sql
@@ -1,4 +1,9 @@
 USE company;
--- SELECT DISTINCT Department FROM worker;
+SELECT DISTINCT Department FROM worker;
 -- GROUP BY
-SELECT DEPARTMENTK, COUNT(*) FROM worker GROUP BY DEPARTMENT;
+-- SELECT DEPARTMENT, COUNT(*) FROM worker GROUP BY DEPARTMENT;
+-- SELECT department, COUNT(department) FROM worker GROUP BY department;
+-- AVG SALARY OR ALL EMPLOYEE BY EVERY DEPARTMENT
+SELECT department, AVG(salary) FROM worker GROUP BY department;
+-- MINIMUM SALARY 
+SELECT department, MIN(salary) FROM worker GROUP BY department;
