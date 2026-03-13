dc0bf8bfb9791b58de61241171841a8fcb38ee32
Nikhil Sharma <159870552+Nikhil9Sharma@users.noreply.github.com>
2026-03-13 21:03:32 +0530

    Filtering Data Learning

--- DataWithBaraa/Filtering Data Using Comparison Operator.sql
+++ DataWithBaraa/Filtering Data Using Comparison Operator.sql
@@ -1,4 +1,5 @@
 Use mydatabase;
+/*
 -- Simple Operators
 SELECT * FROM customers WHERE country = "Germany";
 SELECT * FROM customers WHERE country != "Germany";
@@ -13,4 +14,16 @@ SELECT * FROM customers WHERE NOT score < 500;
 SELECT * FROM customers WHERE score BETWEEN 100 AND 500 ;
 
 -- MEMBERSHIP OPERATORS IN AND NOT IN 
-SELECT * FROM customers WHERE SCORE IN 350;
+SELECT * FROM customers WHERE score IN (350,500);
+
+SELECT * FROM customers WHERE country IN ('Germany','USA');
+
+*/
+-- Search Operator LIKE 
+
+SELECT * FROM customers WHERE first_name LIKE '%n%';
+SELECT * FROM customers WHERE first_name LIKE '%r%';
+
+SELECT * FROM customers WHERE first_name LIKE '__r%';
+
+
