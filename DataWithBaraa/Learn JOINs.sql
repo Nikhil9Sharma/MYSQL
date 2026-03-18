69db2925506e1dab58b1ceed72e5fa249d774233
Nikhil Sharma <159870552+Nikhil9Sharma@users.noreply.github.com>
2026-03-18 20:34:32 +0530

    Learn Right JOINS and FULL JOINS

--- DataWithBaraa/Learn JOINs.sql
+++ DataWithBaraa/Learn JOINs.sql
@@ -16,14 +16,51 @@ SELECT c.id,
 FROM customers AS c
 INNER JOIN orders AS o
 ON id = customer_id ; 
-*/
+
 SELECT c.id,
-		c.first_name,
-        c.country,
+		c.country,
         o.order_id,
         o.sales
 FROM customers AS c
 LEFT JOIN orders AS o 
 ON id = customer_id;
 
--- SELECT * FROM orders; 
+-- Right JOINS ---
+
+SELECT c.id,
+		c.country,
+        o.order_id,
+        o.sales
+FROM customers AS c
+RIGHT JOIN orders AS o 
+ON id = customer_id;
+
+SELECT c.id,
+		c.country,
+        o.order_id,
+        o.sales
+        
+FROM orders AS o
+LEFT JOIN customers AS c 
+ON id = customer_id;
+
+SELECT 
+    c.id,
+    c.first_name,
+    o.order_id,
+    o.sales
+FROM orders AS o
+FULL JOIN customers AS c 
+    ON c.id = o.customer_id;
+    
+-- Step 1: Get all records from the LEFT table (orders) and matching customers
+SELECT c.id, c.first_name, o.order_id, o.sales
+FROM orders AS o
+LEFT JOIN customers AS c ON c.id = o.customer_id
+UNION
+-- Step 2: Get all records from the RIGHT table (customers) and matching orders
+SELECT c.id, c.first_name, o.order_id, o.sales
+FROM orders AS o
+RIGHT JOIN customers AS c ON c.id = o.customer_id;
+*/
+
