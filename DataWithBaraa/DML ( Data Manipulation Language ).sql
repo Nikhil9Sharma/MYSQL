f44ef9d5b72754a14a5fb27530debcf7d56a13a4
Nikhil Sharma <159870552+Nikhil9Sharma@users.noreply.github.com>
2026-03-19 20:57:14 +0530

    Refine the DML File

--- DataWithBaraa/DML ( Data Manipulation Language ).sql
+++ DataWithBaraa/DML ( Data Manipulation Language ).sql
@@ -1,12 +1,12 @@
 USE mydatabase;
-/*
-INSERT INTO customers (id, first_name) 
-	VALUES 
+
+INSERT INTO customers (id, first_name)
+	VALUES
     (10, 'SARA');
 SELECT * FROM customers;
-*/
+
 -- INSERT DATA FROM 'CUSTOMER' INTO 'PERSONS'
-/*
+
 INSERT INTO persons (id, persons_name, dob, phone)
 SELECT id, first_name, NULL, 'Unknown'
 FROM customers;
@@ -16,26 +16,26 @@ SHOW COLUMNS FROM persons;
 INSERT INTO persons (id, person_name, dob, phone)
 SELECT id, first_name, NULL, 'Unknown'
 FROM customers;
-  -- CHANGE THE SCORE OF CUSTOMER NUMBER 6 TO 0
-UPDATE 
+-- CHANGE THE SCORE OF CUSTOMER NUMBER 6 TO 0
+UPDATE
 customers
- SET score = 0 
+SET score = 0
 	WHERE id = 6;
- -- CHANGE THE SCORE OF CUSTOMER NUMBER 10 TO 0 AND Country is UK
-UPDATE 
+-- CHANGE THE SCORE OF CUSTOMER NUMBER 10 TO 0 AND Country is UK
+UPDATE
 customers
- SET score = 0, country = 'UK' 
+SET score = 0, country = 'UK'
 	WHERE id = 10;
 -- -----------------------
- UPDATE customers
+UPDATE customers
 SET score = 0
 WHERE score IS NULL AND id > 0;
 
 
 -- DELETE ALL CUSTOMERS WITH AN ID GREATER THAN 5
-DELETE FROM customers 
+DELETE FROM customers
 WHERE ID > 5;
 
- */
+
 TRUNCATE TABLE persons;
 SELECT * FROM customers;
