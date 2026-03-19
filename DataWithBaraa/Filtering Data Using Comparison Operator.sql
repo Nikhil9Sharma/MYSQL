8bcd0df2278af753e9b14c229c3089946886d7e3
Nikhil Sharma <159870552+Nikhil9Sharma@users.noreply.github.com>
2026-03-19 20:58:29 +0530

    Refine the Filtering Data File

--- DataWithBaraa/Filtering Data Using Comparison Operator.sql
+++ DataWithBaraa/Filtering Data Using Comparison Operator.sql
@@ -1,25 +1,25 @@
 Use mydatabase;
-/*
+
 -- Simple Operators
 SELECT * FROM customers WHERE country = "Germany";
 SELECT * FROM customers WHERE country != "Germany";
 SELECT * FROM customers WHERE country <> "Germany";
 
--- Logical Comparison Operator  
+-- Logical Comparison Operator
 SELECT * FROM customers WHERE country = "Germany" AND score > 500;
 SELECT * FROM customers WHERE country = "Germany" OR score > 500;
 SELECT * FROM customers WHERE NOT score < 500;
 
---  RANGE OPERATORS 
+--  RANGE OPERATORS
 SELECT * FROM customers WHERE score BETWEEN 100 AND 500 ;
 
--- MEMBERSHIP OPERATORS IN AND NOT IN 
+-- MEMBERSHIP OPERATORS IN AND NOT IN
 SELECT * FROM customers WHERE score IN (350,500);
 
 SELECT * FROM customers WHERE country IN ('Germany','USA');
 
 */
--- Search Operator LIKE 
+-- Search Operator LIKE
 
 SELECT * FROM customers WHERE first_name LIKE '%n%';
 SELECT * FROM customers WHERE first_name LIKE '%r%';
