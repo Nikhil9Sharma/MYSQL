118893203442684bcb5dc16f69a75774e1180103
Nikhil Sharma <159870552+Nikhil9Sharma@users.noreply.github.com>
2026-03-19 20:55:28 +0530

    Refine DDL File

--- DataWithBaraa/DDL(data Definition Language).sql
+++ DataWithBaraa/DDL(data Definition Language).sql
@@ -1,5 +1,4 @@
--- Create a new table called persons
- -- with columns : id, person_name, dob, and phone
+
 USE mydatabase;
 CREATE TABLE IF NOT EXISTS persons(
 	id INT NOT NULL AUTO_INCREMENT,
@@ -8,11 +7,4 @@ CREATE TABLE IF NOT EXISTS persons(
     phone VARCHAR(15) NOT NULL,
     CONSTRAINT pk_persons PRIMARY KEY(id)
 );
--- ADD NEW COLUMN EMAIL IN PERSONS TABLE 
-/*
-ALTER TABLE persons
-	ADD phone varchar(15) NOT NULL;
-ALTER TABLE persons
-	ADD email VARCHAR(50) NOT NULL;
-*/
 SELECT * FROM persons;
