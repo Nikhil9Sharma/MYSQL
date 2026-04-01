eecfe44efe5257dab106acffc7f16dca45b12e6b
Nikhil Sharma <159870552+Nikhil9Sharma@users.noreply.github.com>
2026-04-01 22:15:58 +0530

    Today i Learn the UNIION

---
+++ DataWithBaraa/Set Operations.sql
@@ -0,0 +1,17 @@
+USE Salesdb;
+
+(SELECT c.customerID,
+		c.FirstName,
+        c.LastName
+ FROM customers As c)
+UNION 
+(SELECT e.employeeID,
+		e.FirstName,
+        e.LastName
+ FROM employees AS e);
+
+
+
+
+
+
