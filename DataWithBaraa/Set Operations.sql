7f7bbe3207740c8f7b618a0e02f280c8b2857965
Nikhil Sharma <159870552+Nikhil9Sharma@users.noreply.github.com>
2026-04-07 20:38:20 +0530

    Learn the UNION and UNION ALL

--- DataWithBaraa/Set Operations.sql
+++ DataWithBaraa/Set Operations.sql
@@ -10,13 +10,29 @@ c.FirstName,
 c.LastName
 FROM Salesdb.Customers AS c;
 SELECT
+'Employeees' AS SourceTable,
 e.EmployeeID,
 e.FirstName,
 e.LastName
 FROM salesdb.Employees AS e
 UNION ALL
 SELECT
+'Customers' AS SourceTable,
 c.CustomerID,
 c.FirstName,
 c.LastName
 FROM Salesdb.Customers AS c;
+
+SELECT `ordersarchive`.`OrderID`,
+    `ordersarchive`.`ProductID`,
+    `ordersarchive`.`CustomerID`,
+    `ordersarchive`.`SalesPersonID`,
+    `ordersarchive`.`OrderDate`,
+    `ordersarchive`.`ShipDate`,
+    `ordersarchive`.`OrderStatus`,
+    `ordersarchive`.`ShipAddress`,
+    `ordersarchive`.`BillAddress`,
+    `ordersarchive`.`Quantity`,
+    `ordersarchive`.`Sales`,
+    `ordersarchive`.`CreationTime`
+FROM `salesdb`.`ordersarchive`;
